81fa33d3b00882150be4286a54bf1925e0d9ca71
mbwilding <mbwilding@gmail.com>
2021-08-23 11:36:03 +0800

    ADDED: Application open time decreased

--- PacketTracerConfigUnlock/Targets.cs
+++ PacketTracerConfigUnlock/Targets.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace PacketTracerConfigUnlock
@@ -11,33 +12,45 @@ namespace PacketTracerConfigUnlock
             (
                 "Cisco Packet Tracer 7.2.2 (x86)",
                 @"C:\Program Files (x86)\Cisco Packet Tracer 7.2.2\bin\PacketTracer7.exe",
-                "E92B0100",
-                0x1063AA4
+                new Dictionary<int, string>()
+                {
+                    {0x1063AA4, "E92B0100"},    // Unlock config tab
+                    {0x16AAB0D, "E9530100"}     // Speed up loading times
+                }
             );
             Patch
             (
                 "Cisco Packet Tracer 8.0.1 (x86)",
                 @"C:\Program Files (x86)\Cisco Packet Tracer 8.0.1\bin\PacketTracer.exe",
-                "E9380200",
-                0x112EC6D
+                new Dictionary<int, string>()
+                {
+                    {0x112EC6D, "E9380200"},    // Unlock config tab
+                    {0x191806D, "E9FF0600"}     // Speed up loading times
+                }
             );
             Patch
             (
                 "Cisco Packet Tracer 7.2.2 (x64)",
                 @"C:\Program Files\Cisco Packet Tracer 7.2.2\bin\PacketTracer7.exe",
-                "E9490100",
-                0x147B638
+                new Dictionary<int, string>()
+                {
+                    {0x147B638, "E9490100"},    // Unlock config tab
+                    {0x1C58604, "E9650500"}     // Speed up loading times
+                }
             );
             Patch
             (
                 "Cisco Packet Tracer 8.0.1 (x64)",
                 @"C:\Program Files\Cisco Packet Tracer 8.0.1\bin\PacketTracer.exe",
-                "E9FA0200",
-                0x1720076
+                new Dictionary<int, string>()
+                {
+                    {0x1720076, "E9FA0200"},    // Unlock config tab
+                    {0x218B564, "E9820800"}     // Speed up loading times
+                }
             );
         }
 
-        private static void Patch(string name, string path, string payload, int offset)
+        private static void Patch(string name, string path, Dictionary<int, string> payloads)
         {
             if (!File.Exists(path)) { return; }
 
@@ -51,18 +64,23 @@ namespace PacketTracerConfigUnlock
 
             File.Copy(path, path + ".bak");
 
-            var fileStream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
-            fileStream.Seek(offset, SeekOrigin.Begin);
-            var patch = Converters.HexToBytes(payload);
-            try
+            var fileStream = new FileStream(path, FileMode.Open, FileAccess.Write, FileShare.Write);
+
+            foreach (var entry in payloads)
             {
-                fileStream.Write(patch, 0, patch.Length);
+                fileStream.Seek(entry.Key, SeekOrigin.Begin);
+                var patch = Converters.HexToBytes(entry.Value);
+                try
+                {
+                    fileStream.Write(patch, 0, patch.Length);
+                }
+                catch (Exception e)
+                {
+                    Interface.Write(e.ToString(), ConsoleColor.Red);
+                    throw;
+                }
             }
-            catch (Exception e)
-            {
-                Interface.Write(e.ToString(), ConsoleColor.Red);
-                throw;
-            }
+
             fileStream.Dispose();
 
             Interface.Write("Successfully patched " + name + ".", ConsoleColor.Green);
